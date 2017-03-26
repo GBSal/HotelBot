72690813b31d86f7ce4987c200612c213c505179
MSal <salman_it24@hotmail.com>
2017-03-26 17:03:44 +0100

    Add luis api

--- HotelBot/Controllers/MessagesController.cs
+++ HotelBot/Controllers/MessagesController.cs
@@ -9,6 +9,7 @@ using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using Microsoft.Bot.Builder.Dialogs;
 using HotelBot.Dialogs;
+using HotelBot.Models;
 
 namespace HotelBot
 {
@@ -24,7 +25,11 @@ namespace HotelBot
             if (activity.Type == ActivityTypes.Message)
             {
                 //await Conversation.SendAsync(activity, () => new GreetingDialog());
-                await Conversation.SendAsync(activity, () =>  HotelBotDialog.dialog);
+                //await Conversation.SendAsync(activity, () =>  HotelBotDialog.dialog);
+                // To use LUIS API
+
+                await Conversation.SendAsync(activity, MakeLuisDialog);
+
             }
             else
             {
@@ -32,6 +37,11 @@ namespace HotelBot
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        private IDialog<RoomReservation> MakeLuisDialog()
+        {
+            return Chain.From(() => new HotelBotLuisDialog(RoomReservation.BuildForm));
         }
 
         private Activity HandleSystemMessage(Activity message)
---
+++ HotelBot/Dialogs/HotelBotLuisDialog.cs
@@ -0,0 +1,86 @@
+﻿using HotelBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using HotelBot.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace HotelBot.Dialogs
+{
+
+    [LuisModel("7651e348-ae0d-4611-a8e0-0ebf53c8f77b", "b9f635eb5c3d4bacb0270c105b6ef8b7")]
+    [Serializable]
+    public class HotelBotLuisDialog : LuisDialog<RoomReservation>
+    {
+
+        private readonly BuildFormDelegate<RoomReservation> ReserveRoom;
+
+        public HotelBotLuisDialog(BuildFormDelegate<RoomReservation> reserveRoom)
+        {
+           this. ReserveRoom = reserveRoom;
+        }
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("I'm sorry I don't know what you mean.");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            context.Call(new GreetingDialog(), CallBack);
+        }
+
+       
+        [LuisIntent("Reservation")]
+        public async Task RoomReservation(IDialogContext context, LuisResult result)
+        {
+            var enrolForm = new FormDialog<RoomReservation>(new RoomReservation(), 
+                                                                this.ReserveRoom, 
+                                                                FormOptions.PromptInStart);
+            context.Call<RoomReservation>(enrolForm, CallBack);
+        }
+
+
+        [LuisIntent("QueryAmenities")]
+        public async Task QueryAmenities(IDialogContext context, LuisResult result)
+        {
+            foreach (var entity in result.Entities.Where(e => e.Type == "amenity"))
+            {
+                var value = entity.Entity.ToLower();
+                if (value == "pool" || value == "gym" || value == "wifi" || value == "towels")
+                {
+                   await context.PostAsync("Yes we have that!");
+                    context.Wait(MessageReceived);
+                    return;
+
+
+                }
+                else
+                {
+                    await context.PostAsync("I'm sorry I don't have that");
+                    context.Wait(MessageReceived);
+                    return;
+                }
+
+            }
+            await context.PostAsync("I'm sorry I don't have that");
+            context.Wait(MessageReceived);
+            return;
+        }
+
+
+        private async Task CallBack(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Wait(MessageReceived);
+        }
+
+    }
+}
